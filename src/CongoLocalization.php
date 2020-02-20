48355e4638998ac36e1d07175505c0e96437dcfc
Igor GAMATH <ekolo.openeducation@gmail.com>
2020-02-20 23:36:30 +0100

    Create CongoLocalization.php
    Add the new class CongoLocalization

---
+++ src/CongoLocalization.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the congo/localization library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ig.gamath@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://packagist.org/packages/ramsey/uuid Packagist
+ * @link https://github.com/IAICongoCommunity/congo-localization GitHub
+ */
+
+namespace Congo\Localization;
+
+
+/**
+ * Represents the different city of Congo country
+ *
+ * @link http://tools.ietf.org/html/rfc4122
+ * @link http://en.wikipedia.org/wiki/Universally_unique_identifier
+ * @link http://docs.python.org/3/library/uuid.html
+ * @link http://docs.oracle.com/javase/6/docs/api/java/util/UUID.html
+ */
+class CongoLocalization{
+
+}
