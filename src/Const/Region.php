0ba4ec8d3d5ab78e0106747bbb53f2717bbbe1f9
Igor GAMATH <ekolo.openeducation@gmail.com>
2020-02-21 07:03:04 +0100

    Create Region.php
    Add Region class

---
+++ src/Const/Region.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the congo/localization library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ig.gamath@gmail.com>
+ * @license https://packagist.org/packages/congo/localization MIT
+ * @link https://packagist.org/packages/congo/localization Packagist
+ * @link https://github.com/IAICongoCommunity/congo-localization GitHub
+ */
+
+namespace Congo\Localization\Const;
+
+
+/**
+ * Represents the different Neighbourhood of Congo country
+ *
+ */
+abstract class Region{
+	
+	const BRAZZAVILLE = "Brazzaville";
+	const POINTE_KOUILOU = "Pointe-Kouilou";
+	const NIARI = "Niari";
+	const BOUENZA = "Bouenza";
+	const KOUILOU = "Kouilou";
+	const SANGHA = "Sangha";
+	const CUVETTE = "Cuvette";
+	const CUVETTE_OUEST = "Cuvette-Ouest";
+	const PLATEAUX = "Plateaux";
+	const LIKOUALA = "Likouala";
+	const LEKOUMOU = "Lékoumou";
+	const POOL = "Pool";
+}
