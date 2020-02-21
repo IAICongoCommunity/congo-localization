8be14e194081e084cfe43c77c289c02568b6b11d
Igor GAMATH <ekolo.openeducation@gmail.com>
2020-02-21 06:32:56 +0100

    Create Neighbourhood.php
    Add Neighbourhood class

---
+++ src/Const/Neighbourhood.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the congo/localization library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ig.gamath@gmail.com>
+ * @license https://packagist.org/packages/congo/localization MIT
+ * @link https://packagist.org/packages/congo/localization Packagist
+ * @link https://github.com/IAICongoCommunity/congo-localization GitHub
+ */
+
+namespace Congo\Localization\Const;
+
+
+/**
+ * Represents the different Neighbourhood of Congo country
+ *
+ */
+abstract class Neighbourhood{
+	
+}
